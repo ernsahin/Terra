83d7c35f6d84461b0aa3d5c64942470ef070516d
ernsahin <93529206+ernsahin@users.noreply.github.com>
2023-01-05 02:14:36 +0300

    Delete streamlit_app.py

--- TerraMega/streamlit_app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.sidebar()
-st.image(image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.btchaber.com%2Fterra-projesi-nedir%2F&psig=AOvVaw3RDEefd7qtePvHU1CtFzVA&ust=1672959607662000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCICDsqyCr_wCFQAAAAAdAAAAABAD")
-st.header("Welcome to the Mega TerraDashboard")
